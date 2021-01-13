e8bc91abb46b7658a7ae220b6633b939b190c663
Kaan Er <erkaan1@outlook.com>
2021-01-13 21:57:08 +0300

    Add heroku keep awake module

--- src/index.js
+++ src/index.js
@@ -43,6 +43,11 @@ const renderApp = () => {
   }
 };
 
+var http = require("http");
+setInterval(function () {
+  http.get("http://kodlayanblog.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 store.dispatch(getRolesFromDatabase());
 
 store.dispatch(getCategoriesFromDatabase());
